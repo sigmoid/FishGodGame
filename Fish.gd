1ca83bfc7eb43b70778e3dd1b307703203f199bf
Adam Waggoner <adam.swaggoner@gmail.com>
2024-05-05 16:30:33 -0500

    Add attraction to fish

--- Fish.gd
+++ Fish.gd
@@ -8,6 +8,9 @@ var max_speed = 10
 @export 
 var distance_threshold = 0.2
 
+@export 
+var negative_distance_threshold = 100
+
 @export
 var rotate_duration = 0.5
 
@@ -24,6 +27,8 @@ var current_velocity = 0.0
 @export
 var acceleration = 10.0
 
+var attraction = 0.0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -38,19 +43,26 @@ func _process(delta):
 		rotate_timer -= delta
 		
 	if(current_velocity < max_speed):
-		current_velocity += acceleration * delta
+		current_velocity += acceleration * delta * abs(attraction)
 		
 		if(current_velocity > max_speed):
 			current_velocity = max_speed
 		
-	if self.position.distance_to(target_pos) > distance_threshold:
+	if (attraction > 0 and self.position.distance_to(target_pos) > distance_threshold) || (attraction < 0 && self.position.distance_to(target_pos) > negative_distance_threshold):
 		self.position += (target_pos - self.position).normalized() * current_velocity * delta
 	else:
 		current_velocity = 0.0
 		
-func setTargetPosition(pos):
+func setTargetPosition(pos, attr):
 	target_pos = pos
+	
 	target_rotation = target_pos.angle_to_point(self.position)
+	
+	self.attraction = attr
+	
+	if(attraction < 0):
+		target_rotation += PI
+	
 	prev_rotation = self.rotation
 	rotate_timer = rotate_duration
 	
