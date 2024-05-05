cd3dc46e90f8f9d779afee3cd838d9668d5dccb6
skeeth <ethanhalbe@gmail.com>
2024-05-05 15:54:57 -0500

    More Spawning, on a timer

--- Spawner/Spawner.gd
+++ Spawner/Spawner.gd
@@ -5,7 +5,8 @@ class_name Spawner
 @export var spawn_locations:Array[SpawnLocation]
 @export var parent:Node
 var packed_scene:PackedScene
-
+@export var maxBatchSize:int
+@export var maxInterval:float
 func _ready():
 	assert(path != "", "Item path must but set.")
 	assert(parent != null, "parent must be set in the inspector.")
@@ -23,3 +24,12 @@ func _input(event):
 	if event.is_action_pressed("test"):
 		spawn()
 
+@onready var timer = $Timer
+
+
+func _on_timer_timeout():
+	var batchSize = randi_range(1,maxBatchSize)
+	for i in range(0,batchSize):
+		spawn()
+		timer.wait_time = randf_range(0.3,maxInterval)
+	pass # Replace with function body.
