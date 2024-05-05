6711d8dca8be649f2e80e7dad4fe1189b2523b3f
skeeth <ethanhalbe@gmail.com>
2024-05-05 15:44:00 -0500

    Item Attachments

--- Item/Item.gd
+++ Item/Item.gd
@@ -13,6 +13,7 @@ Color.GREEN,Color.SKY_BLUE,Color.PURPLE,Color.GOLDENROD]
 
 @export var collision_shape_2d:CollisionShape2D
 var attach_target:Node2D
+var velocity:Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,6 +24,9 @@ func _draw():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	position += velocity
+	if attach_target:
+		velocity -= (position - attach_target.position).normalized()
 	pass
 
 
@@ -33,4 +37,7 @@ func _on_area_entered(area):
 				tier = (tier + 1) % 5
 			else:
 				queue_free()
+	else:
+		if area is FishHitbox:
+			attach_target = area.get_parent()
 	pass # Replace with function body.
